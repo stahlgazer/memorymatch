12770cb71c7d2c40cfa333d40b4a8927d1f90e34
Gavin S. <stahlgavin@gmail.com>
2023-09-20 15:16:57 -0700

    basic styles and description

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [level, setLevel] = useState(1);
   const [sequence, setSequence] = useState([]);
   const [userInput, setUserInput] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('Press Start For New Game');
   const [isPlaying, setIsPlaying] = useState(false);
   const [inSequence, setInSequence] = useState(false);
 
@@ -17,17 +17,21 @@ function App() {
     }
   }, [isPlaying, level]);
 
+  const startGame = () => {
+    setUserInput([]);
+    setMessage('Watch carefully...');
+    generateSequence(level); // Generate the sequence here
+    setIsPlaying(true);
+  };
+
   const generateSequence = (level) => {
     const newSequence = [];
-
     for (let i = 0; i < level; i++) {
       const randomColor = COLORS[Math.floor(Math.random() * COLORS.length)];
       newSequence.push(randomColor);
     }
     setSequence(newSequence);
-
   };
-
 
   const playSequence = async () => {
     setInSequence(true);
@@ -41,11 +45,15 @@ function App() {
     setInSequence(false);
   };
 
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
   const lightUpSquare = (color) => {
     return new Promise((resolve) => {
       setMessage(`Watch carefully: ${color}`);
       setTimeout(() => {
-        document.getElementById(color).style.backgroundColor = 'white';
+        document.getElementById(color).style.backgroundColor = 'lightgray';
         setTimeout(() => {
           resolve();
         }, 500);
@@ -62,9 +70,7 @@ function App() {
     });
   };
 
-  const sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
+
 
   const handleSquareClick = (color) => {
     if (!isPlaying) return; // Ignore clicks when not playing
@@ -76,26 +82,18 @@ function App() {
         // User completed the level
         setUserInput([]);
         setLevel(level + 1);
-        setMessage('Good job! Next level.');
+        setMessage('Good job! Next level');
         setIsPlaying(false);
 
       }
     } else {
       // User made a mistake, end the game
+      setUserInput([]);
+      setLevel(1);
+      setMessage('Press Start For New Game');
+      alert('You lost! Please Try Again.')
       setIsPlaying(false);
-      setMessage('Oops! You lost.');
-      setTimeout(() => {
-        setUserInput([]);
-        setLevel(1);
-      }, 1000);
     }
-  };
-
-  const startGame = () => {
-    setUserInput([]);
-    setMessage('Watch carefully...');
-    generateSequence(level); // Generate the sequence here
-    setIsPlaying(true);
   };
 
   const renderSquares = () => {
@@ -112,13 +110,33 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Memory Game</h1>
+      <h1>Memory Match</h1>
+      <p>Level: {level}</p>
       <button onClick={startGame} disabled={isPlaying}>
         Start
       </button>
-      <p>Level: {level}</p>
-      <div className="board">{renderSquares()}</div>
       <p>{message}</p>
+      <div className="board">{renderSquares()}
+      </div>
+      <div style={{ width: '100%', textAlign: 'left' }}>
+        <p>Welcome to Memory Match, a challenging and fun memory game that will put your skills to the test! Can you rise through the levels and become the ultimate Memory Master?
+
+          <h2>How to Play</h2>
+          Click the &quot;Start&quot; button to begin the game.
+          Pay close attention to the sequence of colored squares that light up.
+          Your challenge is to remember and click on the squares in the exact same order once the sequence ends.
+          As you progress through the levels, the sequences will become longer and more challenging.
+          Complete each level to unlock the next, and see how far you can go!
+          If you make a mistake, don&apos;t worry; you can always start over and try to beat your best score.
+
+          <h2>Tips</h2>
+          Focus and stay sharp! The sequences get trickier as you advance.
+          Take your time. There&apos;s no rush, so make sure you get it right!
+          Challenge your friends to see who can achieve the highest level and become the true Memory Master.
+
+          Are you ready to test your memory and become the Memory Master? Click &quot;Start&quot; and find out!</p>
+      </div>
+
     </div>
   );
 }
